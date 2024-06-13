d0d25278bf9f9721eb07853422daef4bf569c41a
clotodex <clotodex@online.de>
2024-06-13 13:04:24 +0200

    wip: networking not working

--- config/networking.nix
+++ config/networking.nix
@@ -4,10 +4,44 @@
 }: {
   systemd.network.enable = true;
 
+  # boot.initrd.systemd.network = {
+  #   enable = true;
+  #   networks = {inherit (systemd.network.networks) "10-lan1";};
+  # };
+
+  systemd.network.networks = {
+    "10-lan1" = {
+      DHCP = "yes";
+      # matchConfig.MACAddress = config.repo.secrets.local.networking.interfaces.lan1.mac;
+      networkConfig = {
+        IPv6PrivacyExtensions = "yes";
+        # MulticastDNS = true;
+      };
+      dhcpV4Config.RouteMetric = 10;
+      dhcpV6Config.RouteMetric = 10;
+    };
+    "10-wlan1" = {
+      DHCP = "yes";
+      # matchConfig.MACAddress = config.repo.secrets.local.networking.interfaces.wlan1.mac;
+      networkConfig = {
+        IPv6PrivacyExtensions = "yes";
+        # MulticastDNS = true;
+      };
+      dhcpV4Config.RouteMetric = 40;
+      dhcpV6Config.RouteMetric = 40;
+    };
+  };
+
+  # networking.nftables.firewall = {
+  #   zones.untrusted.interfaces = ["lan1" "wlan1"];
+  # };
+
   networking = {
+    # inherit (config.repo.secrets.local.networking) hostId;
     useDHCP = lib.mkForce false;
     useNetworkd = true;
     dhcpcd.enable = false;
+    wireless.iwd.enable = true;
   };
 
   services.resolved = {
