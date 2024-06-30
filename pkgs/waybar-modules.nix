0897964c8336dfd13dc6003ceb6e7553d78b20e3
clotodex <clotodex@online.de>
2024-06-30 23:56:23 +0200

    fix: rename waybar modules to not conflict with system commands like mem

--- pkgs/waybar-modules.nix
+++ pkgs/waybar-modules.nix
@@ -9,7 +9,17 @@
 in
   naersk'.buildPackage {
     src = inputs.waybar-custom-modules-src;
-    nativeBuildInputs = with pkgs; [ pkg-config dbus ];
-    buildInputs = with pkgs; [ dbus udev ];
-    cargoBuildOptions = x: x ++ [ "--workspace" "--exclude" "swaync-client" "--exclude" "swaync"];
+    nativeBuildInputs = with pkgs; [pkg-config dbus];
+    buildInputs = with pkgs; [dbus udev];
+    cargoBuildOptions = x: x ++ [
+      "--workspace"
+      "--exclude" "swaync-client"
+      "--exclude" "swaync"
+      "--exclude" "webcam"
+      ];
+    postInstall = ''
+      mv $out/bin/cpu $out/bin/waybar-cpu
+      mv $out/bin/mem $out/bin/waybar-mem
+      mv $out/bin/cpufreq $out/bin/waybar-cpurfeq
+    '';
   }
