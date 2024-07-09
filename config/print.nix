48ecda85a374fb323b0edb86e59c017760d1c866
clotodex <clotodex@online.de>
2024-07-09 12:18:41 +0200

    feat: printing drivers

--- config/print.nix
+++ config/print.nix
@@ -4,7 +4,12 @@
   ...
 }: {
   # Enable CUPS to print documents.
-  services.printing.enable = true;
-
+  services.printing = {
+    enable = true;
+    extraConf = ''
+      DefaultEncryption Never
+    '';
+    drivers = with pkgs; [ gutenprint hplip splix brlaser ];
+  };
   # TODO: install wild printers
 }
