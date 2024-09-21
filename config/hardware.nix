c1c5bf6ffde7ee4bcea18e817049977866ee82fa
clotodex <clotodex@online.de>
2024-09-21 15:14:48 +0200

    feat: asus specific hw-services

--- config/hardware.nix
+++ config/hardware.nix
@@ -25,4 +25,18 @@
     smartd.enable = true;
     thermald.enable = builtins.elem config.nixpkgs.hostPlatform.system ["x86_64-linux"];
   };
+
+  services.supergfxd.enable = true;
+  services.asusd.enable = true;
+  #services.supergfxd.settings = {
+  #    # mode = "Integrated";
+  #    vfio_enable = true;
+  #    vfio_save = false;
+  #    always_reboot = false;
+  #    no_logind = false;
+  #    logout_timeout_s = 20;
+  #    hotplug_type = "Asus";
+  #  };
+  #};
+  #systemd.services.supergfxd.path = [ pkgs.kmod pkgs.pciutils ];
 }
