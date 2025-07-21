ac50c7a34dad8a0d57918ea8f271cc6845eec588
clotodex <clotodex@online.de>
2025-07-21 22:48:26 +0200

    fix: mpv nvidia

--- config/nvidia.nix
+++ config/nvidia.nix
@@ -5,9 +5,12 @@
   ...
 }:
 lib.optionalAttrs (!minimal) {
-  boot.blacklistedKernelModules = ["nouveau"];
-  services.xserver.videoDrivers = lib.mkForce ["nvidia"];
+  boot.blacklistedKernelModules = [ "nouveau" ];
+  services.xserver.videoDrivers = lib.mkForce [ "nvidia" ];
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
   hardware = {
     nvidia = {
       modesetting.enable = true;
@@ -27,9 +30,15 @@ lib.optionalAttrs (!minimal) {
       driSupport = true;
       driSupport32Bit = true;
       extraPackages = with pkgs; [
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        libvdpau-va-gl
         vaapiVdpau
         nvidia-vaapi-driver
       ];
     };
   };
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  }; # Force intel-media-driver
 }
--- home/media.nix
+++ home/media.nix
@@ -3,6 +3,14 @@
   lib,
   pkgs,
   ...
-}: {
+}:
+{
   services.mpris-proxy.enable = true;
+
+  xdg.configFile."mpv/mpv.conf".text = ''
+    hwdec=auto-safe
+    vo=gpu
+    profile=gpu-hq
+    gpu-context=wayland
+  '';
 }
