4a4c14b72a30044b798603f5443cb0faa16ccec1
clotodex <clotodex@online.de>
2025-10-11 17:40:18 +0200

    flake.lock update (2025-10-11)

--- config/packages.nix
+++ config/packages.nix
@@ -36,5 +36,19 @@
     rustc
     cargo
     kitty.terminfo
+
+    freerdp3
+
+    # support both 32-bit and 64-bit applications
+    wineWowPackages.stable
+    # wine-staging (version with experimental features)
+    wineWowPackages.staging
+    # winetricks (all versions)
+    winetricks
+    # native wayland support (unstable)
+    wineWowPackages.waylandFull
+    #vcrun2015
+    #dotnet48
+    dxvk
   ];
 }
